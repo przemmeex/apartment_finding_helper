5bc7d8fddb2ec5e08c4b527ad526859176a42a71
przemmex <przemmex@o2.pl>
2019-03-11 21:42:57 +0100

    Adding date to save name, improved location dictionary

--- apartment_finding_helper.py
+++ apartment_finding_helper.py
@@ -1,36 +1,40 @@
+# -*- coding: utf-8 -*-
 import json
 import re
+from datetime import datetime
 from mpl_toolkits.mplot3d import Axes3D
+
 import bs4
 import matplotlib.patheffects as path_effects
 import matplotlib.pyplot as plt
 import requests
 
-
 CITY = "wroclaw"
 DEBUG_MODE = True
 SHOW_LABELS = True
 SAVE_DATA = True
-USE_OFFLINE = False
-SAVE_NAME = "apartments_data.json"
+OFFLINE_DATA = ""
+SAVE_NAME = "apartments_data_{}_{}.json".format(datetime.now().timetuple().tm_yday,
+                                                datetime.now().strftime("%H_%M"))
 
-location_rate = {"dolnośląskie": 5, "grabiszynek": 75, "krzyki": 70, "śródmieście": 100,
+location_rate_w = {"dolnośląskie": 5, "grabiszynek": 75, "krzyki": 70, "śródmieście": 100,
                    "centrum": 100, "lipa piotrowska": 10, "sępolno": 55, "gaj": 80, "ołbin": 75,
                    "kleczków": 70, "wojnów": 20, "popowice": 80, "stare miasto": 100,
-                   "bieńkowice": 10, "muchobór mały":65, "pilczyce":70,"południe":15,
+                   "bieńkowice": 10, "muchobór mały": 65, "pilczyce": 70, "południe": 15,
                    "fabryczna": 75, "grabiszyn": 85, "zacisze": 90, "borek": 85, "kłokoczyce": 5,
                    "gądów mały": 75, "księże wielkie": 60, "księże małe": 65, "jagodno": 45,
                    "oporów": 55, "klecina": 55, "poświętne": 40, "nowy dwór": 70, "partynice": 60,
                    "maślice": 65, "psie pole": 35, "karłowice": 45, "tarnogaj": 65, "złotniki": 50,
                    "stabłowice": 45, "żerniki": 45, "szczepin": 90, "biskupin": 85, "brochów": 30,
                    "swojczyce": 40, "kowale": 40, "plac grunwaldzki": 95, "sołtysowice": 35,
-                   "ołtaszyn": 55, "huby": 85, "muchobór wielki":65, "zakrzów":10, "wojszyce": 45}
+                   "ołtaszyn": 55, "huby": 85, "muchobór wielki": 65, "zakrzów": 10, "wojszyce": 45,
+                   "rynek": 100, "kuźniki": 55, "leśnica": 20, "kozanów": 70, "różanka": 60}
 
 main_url = "https://www.otodom.pl/sprzedaz/mieszkanie/{}/".format(CITY)
 
 
 def prepare_data():
-    if not USE_OFFLINE:
+    if not OFFLINE_DATA:
         prices, locations, areas, links = [], [], [], []
         for i in range(1, 10):
             handler = requests.get(main_url, params={"page": str(i)})
@@ -54,7 +58,7 @@ def prepare_data():
                 area_range = re.findall(area_pattern, raw_area.replace(",", "."))
 
                 if location_match and price_match and area_range:
-                    locations.append(location_rate.get(location_match.group(2).lower(), 1))
+                    locations.append(location_rate_w.get(location_match.group(2).lower(), 1))
                     if locations[-1] == 1:
                         print(location_match.group(2))
                     if len(area_range) == 2:
@@ -65,13 +69,13 @@ def prepare_data():
                     prices.append(price_int) if price_int < 750000 else prices.append(750000)
                     links.append(link_url)
     else:
-        with open(SAVE_NAME) as data_file:
+        with open(OFFLINE_DATA) as data_file:
             data_loaded = json.load(data_file)
 
         prices, locations, areas, links = data_loaded["prices"], data_loaded["locations"], \
                                           data_loaded["areas"], data_loaded["links"]
 
-    if SAVE_DATA and not USE_OFFLINE:
+    if SAVE_DATA and not OFFLINE_DATA:
         with open(SAVE_NAME, "w") as file_handler:
             json.dump({"prices": prices, "locations": locations, "areas": areas, "links": links},
                       file_handler, ensure_ascii=False)
